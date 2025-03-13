c2f3db2729caf1f4b642ac263b2a275a6d58fc77
underbieng <sarojkumarm160@gmail.com>
2025-03-13 21:59:10 +0530

    array list

---
+++ ArrayListExample01.java
@@ -0,0 +1,16 @@
+
+import java.util.ArrayList;
+
+public class ArrayListExample01 {
+    public static void main(String[] args) {
+        //syntax
+        ArrayList<Integer> list = new ArrayList<>(10);
+        list.add(23);
+        list.add(3423);
+        System.out.println(list);
+        System.out.println(list.contains(23));
+        list.set(0, 99);
+        list.remove(1);
+        System.out.println(list);
+    }
+}
